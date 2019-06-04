f393ee95781cfb4a888b0404be79b199437d75e4
thealgy <diogosaretta@hotmail.com>
2019-06-03 21:23:57 -0300

    Criação classe Campeonato

---
+++ ArqDota2/Models/Campeonatos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArqDota2.Models
+{
+    public class Campeonatos
+    {
+        public string Duracao { get; set; }
+        public DateTime DataCampeonato { get; set; }
+        
+    }
+}
