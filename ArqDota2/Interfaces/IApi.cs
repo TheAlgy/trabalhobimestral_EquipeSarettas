fdf967b2845508be109a9cd5a8aa90a077831595
Andre Xavier <lordlagon@gmail.com>
2019-06-03 21:51:27 -0300

    cadastro

--- ArqDota2/Interfaces/IApi.cs
+++ ArqDota2/Interfaces/IApi.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using ArqDota2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ArqDota2.Interfaces
 {
-    public interface IApi
+    public interface IApiCampeonato
     {
         Task<IList<Campeonato>> BuscarCampeonatosAtivos();
+        Task<IList<Player>> BuscarPlayers();
+        Task<Campeonato> VisualizarCampeonato();
     }
 }
--- ArqDota2/Services/Repositorio.cs
+++ ArqDota2/Services/Repositorio.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using ArqDota2.Interfaces;
+using ArqDota2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ArqDota2.Services
 {
-    public class Repositorio
+    public class Repositorio : IApiCampeonato
     {
+        public Task<IList<Campeonato>> BuscarCampeonatosAtivos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Player>> BuscarPlayers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Campeonato> VisualizarCampeonato()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
